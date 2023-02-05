4e6b06e9bdbbf0c4a53e954f0af326b1c71f9aa0
Joseph Haryanto <57058570+Jos02378@users.noreply.github.com>
2023-02-05 23:49:06 +0700

    feat: add proxy

--- movie-client/src/api/axiosConfig.js
+++ movie-client/src/api/axiosConfig.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://9c96-103-106-239-104.ap.ngrok.io',
-  headers: { 'ngrok-skip-browser-warning': 'true' },
+  // baseURL: 'http://localhost:8080',
+  headers: {
+    'ngrok-skip-browser-warning': 'true',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
+  },
 });
