c46039dde49afa66c0b3997608ffff78043d0e9e
Joseph Haryanto <57058570+Jos02378@users.noreply.github.com>
2023-02-05 23:52:19 +0700

    feat: add page routes

--- movie-client/src/App.js
+++ movie-client/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import api from './api/axiosConfig';
 import { useState, useEffect } from 'react';
+import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
   const [movies, setMovies] = useState();
@@ -9,12 +12,25 @@ function App() {
     try {
       const response = await api.get('/api/v1/movies');
       console.log(response);
+      setMovies(response.data);
     } catch (error) {
       console.error(error);
     }
   };
 
-  return <div className='App'></div>;
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Home movies={movies} />}></Route>
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
