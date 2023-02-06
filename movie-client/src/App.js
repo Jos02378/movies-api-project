ed9bce7089fb1261ead76bd644a291dae5a3bfd0
Joseph Haryanto <57058570+Jos02378@users.noreply.github.com>
2023-02-07 00:22:32 +0700

    feat: add header and reviews route

--- movie-client/src/App.js
+++ movie-client/src/App.js
@@ -4,9 +4,14 @@ import { useState, useEffect } from 'react';
 import Layout from './components/Layout';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
 
 function App() {
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
 
   const getMovies = async () => {
     try {
@@ -18,15 +23,44 @@ function App() {
     }
   };
 
+  const getMovieData = async (movieId) => {
+    try {
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+
+      const singleMovie = response.data;
+
+      setMovie(singleMovie);
+
+      setReviews(singleMovie.reviewIds);
+
+      console.log(singleMovie);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
     getMovies();
   }, []);
 
   return (
     <div className='App'>
+      <Header />
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route path='/' element={<Home movies={movies} />}></Route>
+          <Route path='/Trailer/:ytTrailerId' element={<Trailer />}></Route>
+          <Route
+            path='/Reviews/:movieId'
+            element={
+              <Reviews
+                getMovieData={getMovieData}
+                reviews={reviews}
+                movie={movie}
+                setReviews={setReviews}
+              />
+            }
+          ></Route>
         </Route>
       </Routes>
     </div>
